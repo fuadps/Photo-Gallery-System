ea9559d2f5fb9467ae9a9172d783fde83cb1c4c1
Fuad <fuadpls@gmail.com>
2019-03-11 23:18:43 +0800

    Create photo.php
    photo class

---
+++ admin/includes/photo.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Photo extends Db_object {
+
+    protected static $db_table = "photos";
+    protected static $db_table_fields = array('photo_id','description','filename','type','size');
+
+    public $photo_id;
+    public $title;
+    public $description;
+    public $filename;
+    public $type;
+    public $size;
+
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = array();
+    public $upload_errors_array = array(
+        0 => 'There is no error, the file uploaded with success',
+        1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+        2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+        3 => 'The uploaded file was only partially uploaded',
+        4 => 'No file was uploaded',
+        6 => 'Missing a temporary folder',
+        7 => 'Failed to write file to disk.',
+        8 => 'A PHP extension stopped the file upload.',
+    );
+
+}
+
+?>
