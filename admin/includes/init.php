40e94d508f297aaeffc6876fc69fc8b89e47570a
Fuad <fuadpls@gmail.com>
2019-03-11 23:18:26 +0800

    Update init.php

--- admin/includes/init.php
+++ admin/includes/init.php
@@ -1,9 +1,16 @@
 <?php
 
+defined('DS') ? null : define('DS',DIRECTORY_SEPARATOR);
+
+define ('SITE_ROOT', DS. 'XAMPP'.DS.'htdocs'.DS.'gallery');
+define ('INCLUDE_PATH',SITE_ROOT.DS.'admin'.DS.'includes');
+
 require_once('functions.php');
 require_once('config.php');
 require_once('database.php');
 require_once('user.php');
+require_once('photo.php');
+require_once('db_object.php');
 require_once('session.php');
 
 ?>
