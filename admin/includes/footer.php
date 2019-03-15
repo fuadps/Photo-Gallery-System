0102d2697100f17e1a60baca300b4970b6b54687
Fuad <fuadpls@gmail.com>
2019-03-15 23:16:22 +0800

    update styles

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,15 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.css" rel="stylesheet">
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
+
+    <script>
+    $(document).ready(function() {
+        $('#summernote').summernote();
+    });
+  </script>
+
 </body>
 
 </html>
--- admin/includes/header.php
+++ admin/includes/header.php
@@ -37,6 +37,9 @@ if (!$session->is_signed_in) {
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
+    <link href="css/styles.css" rel="stylesheet">
+
+
 </head>
 
 <body>
