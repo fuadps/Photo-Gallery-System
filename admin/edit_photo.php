2987ba1074783982979984c07257146be78212e6
Fuad <fuadpls@gmail.com>
2019-03-24 22:16:04 +0800

    Update edit_photo.php

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -77,24 +77,21 @@ if (isset($_POST['update'])) {
                     <div class="col-md-4" >
                             <div  class="photo-info-box">
                                 <div class="info-box-header">
-                                   <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                                   <h4><p class="text-center">Image Info</p></h4>
                                 </div>
                             <div class="inside">
                               <div class="box-inner">
-                                 <p class="text">
-                                   <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
-                                  </p>
                                   <p class="text ">
-                                    Photo Id: <span class="data photo_id_box">34</span>
+                                    Photo Id: <span class="data photo_id_box"><?php echo $photo->photo_id; ?></span>
                                   </p>
                                   <p class="text">
-                                    Filename: <span class="data">image.jpg</span>
+                                    Filename: <span class="data"><?php echo $photo->filename; ?></span>
                                   </p>
                                  <p class="text">
-                                  File Type: <span class="data">JPG</span>
+                                  File Type: <span class="data"><?php echo $photo->type; ?></span>
                                  </p>
                                  <p class="text">
-                                   File Size: <span class="data">3245345</span>
+                                   File Size: <span class="data"><?php echo $photo->size; ?></span>
                                  </p>
                               </div>
                               <div class="info-box-footer clearfix">
