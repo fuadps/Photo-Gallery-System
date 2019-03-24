448ae8f0320253f9ad520f086a985394a22ba675
Fuad <fuadpls@gmail.com>
2019-03-24 22:16:15 +0800

    Update header.php

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -1,12 +1,4 @@
-<?php 
-require_once('init.php');
-
-/*
-if (!$session->is_signed_in) {
-    redirect("login.php");
-}
-*/
-?>
+<?php require_once('init.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
